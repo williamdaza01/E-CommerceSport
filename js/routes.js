5be2ece27ccdf9c3272c02da14aabeed4e51415a
Daza <williamdaza04@gmail.com>
2022-10-10 16:50:31 -0500

    Se crean rutas y router

---
+++ js/routes.js
@@ -0,0 +1,23 @@
+const PATHS = {
+    home: {
+        path: "/"
+    },
+    about: {
+        path: "about"
+    },
+    blog: {
+        path: "blog"
+    },
+    contact: {
+        path: "contact"
+    },
+    shop_single: {
+        path: "shop-sigle"
+    },
+    shop: {
+        path: "shop"
+    },
+    single: {
+        path: "single"
+    }
+}
