79d7fa150cfb340f6808ae7de4593ec4206651bc
Daza <williamdaza04@gmail.com>
2022-10-10 11:08:53 -0500

    Se comienza a implementar autentificacion

--- js/login.js
+++ js/login.js
@@ -1,6 +1,23 @@
-const login = () => {
+const authLogin = () => {
+    const username = localStorage.setItem("username");
+    if(!username) return true;
+    return false;
+}
+
+const saveInfo = () => {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
     localStorage.setItem("username", username);
     localStorage.setItem("password", password);
+}
+
+const login = () => {
+    if(authLogin() == true) {
+        console.log("no exite");
+        alert("El usuario no existe");
+        return true;
+    } else{
+        saveInfo();
+    }
+
 }
