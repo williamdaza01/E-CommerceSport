8ad53d9ebc55b3d47b75282e6f34eac938b5fb6d
jrubio18 <juanpablo.rubio990221@gmail.com>
2022-10-19 02:48:28 -0500

    sigo con problemas en login, cambie metodo

--- js/login.js
+++ js/login.js
@@ -1,26 +1,33 @@
-const authLogin = () => {
-    const username = localStorage.getItem("username");
-    const usernameNew = document.getElementById("username").value;
-    if(username != usernameNew || !usernameNew) return true;
-    return false;
-}
-//probando comit
+let txtuser=document.querySelector(".username");
+let txtpass=document.querySelector(".password");
+let sumbit=document.querySelector(".btnIngresar");
 
-const saveInfo = () => {
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
-    localStorage.setItem("username", username);
-    localStorage.setItem("password", password);
-}
+if(window.localStorage){
 
-const login = () => {
-    console.log(authLogin());
-    if(authLogin() == true) {
-        console.log("no exite");
-        alert("El usuario no existe");
-        return true;
-    } else{
-        saveInfo();
+
+
+//localsotre creando users
+localStorage.setItem("user","wdaza");
+localStorage.setItem("pass","1234");
+
+let user=localStorage.getItem("user");
+let pass=localStorage.getItem("pass");
+
+//let mensaje=document.querySelector(".prueba");
+
+btnIngresar.addEventListener("click",()=>{
+
+    if(user==txtuser.val && pass==txtpass.val){//sigo teniendo problemas con obtener el valor del input
+        
+        console.log("no dio ni pass o user");
+
+    }else{
+
+
+        console.log("no dio ni pass o user");
+
     }
+
+});
 
 }
